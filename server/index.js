3d048b8ba3ebfb9e84f67dd87d2cd26da6b0f8ba
vova <volodymyrtymets@gmail.com>
2019-01-06 13:29:54 +0200

    add api endpoint

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
 const http = require('http');
+const fileSystem = require('fs');
 const express = require('express');
 const path = require('path');
 const app = express();
+const api = express();
 
+api.get('/track', (req, res, err) => {
+  // generate file path
+  const filePath = path.resolve(__dirname, './private', './track.wav');
+  // get file size info
+  const stat = fileSystem.statSync(filePath);
 
+  // set response header info
+  res.writeHead(200, {
+    'Content-Type': 'audio/mpeg',
+    'Content-Length': stat.size
+  });
+  //create read stream
+  const readStream = fileSystem.createReadStream(filePath);
+  // attach this stream with response stream
+  readStream.pipe(res);
+});
+
+//register api calls
+app.use('/api/v1/', api);
 
 // send react app on / GET
 app.use(express.static(path.resolve(__dirname, './public/build/')));
@@ -12,7 +32,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './public/build/', './index.html'));
 });
 
-const server = http.createServer(app, console.log);
+const server = http.createServer(app);
 server.listen('3001', function () {
   console.log('Server app listening on port 3001!');
 });
