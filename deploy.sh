7b1cc75671fc17d3f50646ef6c38644c70456095
volodymyr <volodymyrtymets@gmail.com>
2019-01-17 09:46:34 +0200

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,8 @@ git add ./server/public/
 git commit -m 'build' -n
 
 ## make build commit
-git push -f heroku build:master
+git subtree split --prefix app/api -b heroku-deploy
+git push -f heroku heroku-deploy:master
+git branch -D heroku-api
 
 git checkout master
