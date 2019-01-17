7e6c423646debbdd8add5e5e48557b9ecf6a1110
volodymyr <volodymyrtymets@gmail.com>
2019-01-17 09:23:34 +0200

    build

---
+++ server/public/build/precache-manifest.1d5cff6f905183e2eb60a12be1624802.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "39e066ce84c424640567",
+    "url": "/static/js/main.39e066ce.chunk.js"
+  },
+  {
+    "revision": "6e3a3e91bf78ebb7d0d8",
+    "url": "/static/js/1.6e3a3e91.chunk.js"
+  },
+  {
+    "revision": "39e066ce84c424640567",
+    "url": "/static/css/main.021c2f7f.chunk.css"
+  },
+  {
+    "revision": "6e3a3e91bf78ebb7d0d8",
+    "url": "/static/css/1.49438cff.chunk.css"
+  },
+  {
+    "revision": "f32fef2684f3450652848618051c5290",
+    "url": "/index.html"
+  }
+];
