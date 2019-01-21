db40af0b1d22ff74c4335aa4ac6c24154054aec1
volodymyr <volodymyrtymets@gmail.com>
2019-01-21 09:55:44 +0200

    add nav

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-const Home = () => <h3> Home</h3>;
+const Home = () =>
+  <div className="d-flex justify-content-center flex-column">
+    <h1>Sound in js app exaples</h1>
+    <div className="d-flex flex-column">
+      <Link to="/example1" > Example1: </Link>
+      <Link to="/example2" > Example2: </Link>
+      <Link to="/example3" > Example3: </Link>
+      <Link to="/example4" > Example4: </Link>
+      <Link to="/example5" > Example5: </Link>
+    </div>
+  </div>;
 
 export { Home };
